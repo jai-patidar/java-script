dac191ceb78a73a9a53d68c4aeb319e8755fad12
Jay Patidar <jaipatidar0923@gmail.com>
2025-07-15 06:22:54 +0000

    save change

---
+++ 04_comparision.js/test.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1); best conversion
+
+
+// console.log("2" > 1);
+// console.log("02" > 1); tricky conversion
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0); avoid 
+
+// console.log(undefined == 0); avoid
+
+// ===
+
+// console.log("2" === 2); 
+
+

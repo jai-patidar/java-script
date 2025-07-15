bc681cc770f13e72d66c1017df72b7418e4bf08b
Jay Patidar <jaipatidar0923@gmail.com>
2025-07-15 06:49:57 +0000

    change in location

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,85 @@
+let score = 33  
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// Number => Number 
+// "747sg" => NaN
+// true =>1; false =>0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Jay" => true
+
+let someNumber = 22
+
+let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// ************** Operations ************** //
+
+let value = 5
+let negValue = -value
+//console.log(negValue);
+
+//console.log(5+5);
+//console.log(3-3);
+//console.log(6*6);
+//console.log(8/8);
+//console.log(4**7);
+//console.log(2%3);
+
+
+let str1 = "Hello"
+let str2 = " Jay"
+
+
+let str3 = str1 + str2
+//console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+
+let num1 , num2 , num3 
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+//console.log(gameCounter);
+
+// let x = 3;
+// const y = x++;
+
+// //console.log(`x:${x}, y:${y}`);
+
+// let a = 3;
+// const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+

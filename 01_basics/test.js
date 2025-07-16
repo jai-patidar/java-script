228110047ecd61fa306872e9d7193e4e40673d41
Jay Patidar <jaipatidar0923@gmail.com>
2025-07-16 11:30:49 +0000

    test

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,2 +1,36 @@
-const AccountId = 6874
-console.log(AccountId)
+// Q1
+let a = "Jay"
+let b = 5
+
+console.log(a + b);
+
+
+ // Q2
+console.log(typeof (a + b));
+
+
+// Q3
+const detail = {
+    name : "Jay",
+    age : 22
+}
+
+// detail = 654
+// Q4
+detail['fav lang'] = "python"
+
+console.log(detail);
+
+// Q5
+
+const dict = {
+    burnish :"to make smooth and bright.",
+    ascendant : "The President's party is no longer in the ascendant",
+    yakka : "Hard Work",
+    fabric : "The curtains are made of expensive fabric",
+    aardvark : "a large African animal that has a long nose and that eats ants and other insects"
+}
+
+console.log(dict);
+console.log(dict.yakka);
+

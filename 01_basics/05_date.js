737bd13cc6ea51d7c63378b59b99212204b67d20
Jay Patidar <jaipatidar0923@gmail.com>
2025-09-06 08:18:59 +0000

    Date

---
+++ 01_basics/05_date.js
@@ -0,0 +1,29 @@
+// Dates
+
+let myDate = new Date ()
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date (2025,5,25)
+// let myCreatedDate = new Date (2025, 5, 25, 4)
+// let myCreatedDate = new Date ("2025-05-25")
+let myCreatedDate = new Date ("05-25-2025")
+
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+
+// console.log(newDate);
+// console.log(newDate.getMonth);
+
+
+
+

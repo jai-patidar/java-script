f293be9aa40af0dc71728f13f46865e4c8a98b5d
Jay Patidar <jaipatidar0923@gmail.com>
2025-09-16 17:46:24 +0000

    scops

--- 03_basics/02_scops.js
+++ 03_basics/02_scops.js
@@ -4,14 +4,55 @@ let a = 300
 if (true) {
     let a = 10
     const b = 20
-    console.log("INNER: " , a);
+    // console.log("INNER: " , a);
 }
 
-for (let i = 0 ; i< array.length; i++) {
-    const element = array[i];
+// for (let i = 0 ; i< array.length; i++) {
+//     const element = array[i];
 
-}
+// }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one() {
+    const username = "jai"
+
+    function two() {
+        const website = "learncode"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+
+// one()
+
+if (true) {
+    const username = "jai"
+    if (username === "jai") {
+        const website = " learn"
+        // console.log(username + website);
+    }
+    // console.log(website); error
+    
+}
+// console.log(username); error
+
+
+
+// +++++++++++++++  +++++++++++++++++
+
+addone(5) 
+
+function addone(num) {
+    return num + 1
+}
+
+
+// addTwo(5)
+const addTwo = function (num) {
+    return num + 2
+}
+

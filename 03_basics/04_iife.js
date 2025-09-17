1d05f0c6f2434cb528fb65a0ba4e38acf21c2605
Jay Patidar <jaipatidar0923@gmail.com>
2025-09-17 04:11:50 +0000

    iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,9 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+(function chai(){
+    console.log(`DB connected`);
+})();
+
+( (name) => {
+    console.log(`DB connected ${name}`);
+})(`jai`)
